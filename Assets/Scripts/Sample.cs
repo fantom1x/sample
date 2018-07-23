0e03803ddfea0d4383b1aebfc116f93d772de8e8
fantom1x <fantom_1x@yahoo.co.jp>
2018-07-23 15:25:08 +0900

    Unity project test

---
+++ Assets/Scripts/Sample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Sample : MonoBehaviour {
+
+    // Use this for initialization
+    private void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    private void Update () {
+        
+    }
+}
